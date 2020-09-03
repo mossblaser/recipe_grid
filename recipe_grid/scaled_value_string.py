b94e703f0630b403b6d26639bb5b4d9ea198fab2
Jonathan Heathcote <mail@jhnet.co.uk>
2020-09-03 14:27:10 +0100

    Add ScaledValueString type.

---
+++ recipe_grid/scaled_value_string.py
@@ -0,0 +1,99 @@
+"""
+A string type in which embedded numerical values can be re-scaled.
+
+.. autoclass:: ScaledValueString
+
+"""
+
+from typing import Union, Callable, List, Sequence, Tuple, Any
+
+from collections.abc import Iterable
+
+from fractions import Fraction
+
+from recipe_grid.number_formatting import format_number
+
+Number = Union[int, float, Fraction]
+
+
+__all__ = ["ScaledValueString"]
+
+
+class ScaledValueString:
+    """
+    A string which contains some numerical values which may be re-scaled by a
+    fixed factor.
+
+    This type is very specifically aimed at strings within recipes of the style
+    "divide into 8 burgers about 10cm in diameter". Here when the recipe is
+    scaled up or down, the '8' should be scaled but the '10' should not. Here a
+    :py:class:`ScaledValueString` would be defined like so:
+
+        >>> for_8_burgers = ScaledValueString(
+        ...     ["divide into ", 8, " burgers about 10cm in diameter"]
+        ... )
+
+    This could be scaled up like so:
+
+        >>> for_16_burgers = for_8_burgers.scale(2)
+        >>> for_16_burgers.render()
+        'divide into 16 burgers about 10cm in diameter'
+    """
+
+    _string: Tuple[Union[str, Number], ...]
+
+    def __init__(
+        self, string: Union[str, Number, Sequence[Union[str, Number]]] = ""
+    ) -> None:
+        if not isinstance(string, Iterable):
+            string = [string]
+
+        # Normalise string by combining adjacent strings
+        normalised_string: List[Union[str, Number]] = []
+        for part in string:
+            if (
+                isinstance(part, str)
+                and normalised_string
+                and isinstance(normalised_string[-1], str)
+            ):
+                normalised_string[-1] += part
+            else:
+                normalised_string.append(part)
+
+        self._string = tuple(part for part in normalised_string if part != "")
+
+    def render(self, format_number: Callable[[Number], str] = format_number) -> str:
+        return "".join(
+            part if isinstance(part, str) else format_number(part)
+            for part in self._string
+        )
+
+    def __str__(self) -> str:
+        return self.render()
+
+    def __repr__(self) -> str:
+        return "{}({!r})".format(
+            type(self).__name__,
+            (self._string[0] if len(self._string) == 1 else self._string),
+        )
+
+    def __add__(
+        self, other: Union[str, Number, "ScaledValueString"]
+    ) -> "ScaledValueString":
+        if not isinstance(other, ScaledValueString):
+            other = ScaledValueString(other)
+        return ScaledValueString(self._string + other._string)
+
+    def scale(self, multiplier: Number) -> "ScaledValueString":
+        return ScaledValueString(
+            [
+                part if isinstance(part, str) else part * multiplier
+                for part in self._string
+            ]
+        )
+
+    def __hash__(self) -> int:
+        return hash(self._string)
+
+    def __eq__(self, other: Any) -> bool:
+        return isinstance(other, ScaledValueString) and self._string == other._string
---
+++ tests/test_scaled_value_string.py
@@ -0,0 +1,130 @@
+import pytest
+
+from typing import Union, Sequence, Tuple
+
+from fractions import Fraction
+
+from recipe_grid.scaled_value_string import Number, ScaledValueString
+
+
+@pytest.mark.parametrize(
+    "arg, exp",
+    [
+        # Empty string
+        ("", ()),
+        ([], ()),
+        # Just a string
+        ("foo", ("foo",)),
+        # Just a number
+        (123, (123,)),
+        # A singleton list
+        (["foo"], ("foo",)),
+        ([123], (123,)),
+        # Adjacent strings should be merged
+        (["foo", "bar"], ("foobar",)),
+        # Numbers should be allowed to be interspersed with strings
+        ([123, "foo", "bar"], (123, "foobar")),
+        (["foo", 123, "bar"], ("foo", 123, "bar")),
+        (["foo", "bar", 123], ("foobar", 123)),
+        # Adjacent numbers should not be merged in any way...
+        ([1, 2.0, Fraction(3, 1)], (1, 2.0, Fraction(3, 1))),
+        # Empty strings should be removed
+        (["", 123, ""], (123,)),
+    ],
+)
+def test_constructor(
+    arg: Union[str, Number, Sequence[Union[str, Number]]],
+    exp: Tuple[Union[str, Number], ...],
+) -> None:
+    svs = ScaledValueString(arg)
+    assert svs._string == exp
+
+
+@pytest.mark.parametrize(
+    "arg, exp",
+    [
+        # Just a string
+        ("", ""),
+        ("foo", "foo"),
+        # Just a number
+        (123, "123"),
+        (Fraction(4, 3), "1 1/3"),  # NB: Fancy formatting from number_formatting
+        # Combination of numbers and strings
+        (
+            ["Hello ", 123, " world ", Fraction(4, 3), "! ", 1, 2, 3.0],
+            "Hello 123 world 1 1/3! 123",
+        ),  # NB: Fancy formatting from number_formatting
+    ],
+)
+def test_render(
+    arg: Union[str, Number, Sequence[Union[str, Number]]], exp: str,
+) -> None:
+    svs = ScaledValueString(arg)
+    assert svs.render() == exp
+    assert str(svs) == exp
+
+
+@pytest.mark.parametrize(
+    "a, b, exp_equal",
+    [
+        # Empty
+        (ScaledValueString([]), ScaledValueString([]), True),
+        # Just strings
+        (ScaledValueString("foo"), ScaledValueString("foo"), True),
+        (ScaledValueString("foo"), ScaledValueString("bar"), False),
+        # Just numbers
+        (ScaledValueString(123), ScaledValueString(123), True),
+        (ScaledValueString(123.0), ScaledValueString(123), True),
+        (ScaledValueString(123), ScaledValueString(321), False),
+        # Combination
+        (ScaledValueString(["foo", 1]), ScaledValueString(["foo", 1]), True),
+        (ScaledValueString(["foo", 1]), ScaledValueString(["bar", 1]), False),
+        (ScaledValueString(["foo", 1]), ScaledValueString(["foo", 2]), False),
+        (ScaledValueString("foo1"), ScaledValueString(["foo", 1]), False),
+    ],
+)
+def test_eq(a: ScaledValueString, b: ScaledValueString, exp_equal: bool) -> None:
+    assert (a == b) is exp_equal
+
+
+@pytest.mark.parametrize(
+    "string, mul, exp",
+    [
+        # No numbers
+        (ScaledValueString(), 10, ScaledValueString()),
+        (ScaledValueString("foo"), 10, ScaledValueString("foo")),
+        # Just numbers
+        (ScaledValueString(123), 10, ScaledValueString(1230)),
+        # Combination
+        (ScaledValueString(["foo", 123]), 10, ScaledValueString(["foo", 1230])),
+    ],
+)
+def test_scale(string: ScaledValueString, mul: Number, exp: ScaledValueString) -> None:
+    assert string.scale(mul) == exp
+
+
+@pytest.mark.parametrize(
+    "a, b, exp",
+    [
+        # Add empty
+        (ScaledValueString("foo"), "", ScaledValueString("foo")),
+        (ScaledValueString("foo"), ScaledValueString(), ScaledValueString("foo")),
+        # Add string
+        (ScaledValueString("foo"), "bar", ScaledValueString("foobar")),
+        (ScaledValueString(123), "bar", ScaledValueString([123, "bar"])),
+        # Add numbers
+        (ScaledValueString("foo"), 123, ScaledValueString(["foo", 123])),
+        # Add ScaledValueString
+        (
+            ScaledValueString("foo"),
+            ScaledValueString("bar"),
+            ScaledValueString("foobar"),
+        ),
+    ],
+)
+def test_concat(
+    a: ScaledValueString,
+    b: Union[str, Number, ScaledValueString],
+    exp: ScaledValueString,
+) -> None:
+    assert (a + b) == exp
