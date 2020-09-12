4e951b9adfbb5724dede1df78a3c970c1c5de156
Jonathan Heathcote <mail@jhnet.co.uk>
2020-09-12 15:31:17 +0100

    Add recipe_tree_to_table function.

---
+++ recipe_grid/renderer/recipe_to_table.py
@@ -0,0 +1,108 @@
+"""
+A routine for converting a :py:mod:`recipe_grid.recipe` representation of a
+recipe into a :py:class:`~Table`.
+"""
+
+from typing import cast
+
+from recipe_grid.recipe import (
+    RecipeTreeNode,
+    Ingredient,
+    Reference,
+    Step,
+    SubRecipe,
+)
+
+from recipe_grid.renderer.table import (
+    BorderType,
+    Cell,
+    Table,
+    right_pad_table,
+    combine_tables,
+    set_border_around_table,
+)
+
+
+def recipe_tree_to_table(recipe_tree: RecipeTreeNode) -> Table[RecipeTreeNode]:
+    """
+    Convert a recipe tree into tabular form.
+
+    In the resulting :py:class:`~Table`, cell values are as follows. Firstly,
+    the obvious cases:
+
+    * :py:class:`~ingredient` is shown as is.
+    * :py:class:`~Reference` is shown as its label.
+    * :py:class:`~Step` is shown as its description.
+
+    Finally, :py:class:`~SubRecipe`. These may appear in the table for one of
+    two purposes:
+
+    * For single-output sub recipes they should be rendered as a header row
+      above the sub recipe cells containing the output name as text.
+    * For multiple-output sub-recipes they should be placed in a cell to the
+      right of the sub-recipe and contain (vertically arranged) all of the
+      output names for the sub recipe.
+    """
+    if isinstance(recipe_tree, (Ingredient, Reference)):
+        return Table([[Cell(recipe_tree)]])
+    elif isinstance(recipe_tree, Step):
+        input_tables = [
+            recipe_tree_to_table(input_tree) for input_tree in recipe_tree.inputs
+        ]
+
+        # Pad all input tables to same width and stack them up
+        input_columns = max(table.columns for table in input_tables)
+        input_tables = [right_pad_table(table, input_columns) for table in input_tables]
+        combined_input_tables = combine_tables(input_tables, axis=0)
+
+        # Add step to RHS of inputs
+        return combine_tables(
+            [
+                combined_input_tables,
+                Table.from_dict(
+                    {(0, 0): Cell(recipe_tree, rows=combined_input_tables.rows)}
+                ),
+            ],
+            axis=1,
+        )
+    elif isinstance(recipe_tree, SubRecipe):
+        sub_tree_table = recipe_tree_to_table(recipe_tree.sub_tree)
+
+        if len(recipe_tree.output_names) == 1:
+            return set_border_around_table(
+                combine_tables(
+                    [
+                        Table.from_dict(
+                            {
+                                (0, 0): Cell(
+                                    cast(RecipeTreeNode, recipe_tree),
+                                    columns=sub_tree_table.columns,
+                                ),
+                            }
+                        ),
+                        sub_tree_table,
+                    ],
+                    axis=0,
+                ),
+                BorderType.sub_recipe,
+            )
+        else:
+            return combine_tables(
+                [
+                    set_border_around_table(sub_tree_table, BorderType.sub_recipe),
+                    Table.from_dict(
+                        {
+                            (0, 0): Cell(
+                                cast(RecipeTreeNode, recipe_tree),
+                                rows=sub_tree_table.rows,
+                                border_top=BorderType.none,
+                                border_right=BorderType.none,
+                                border_bottom=BorderType.none,
+                            ),
+                        }
+                    ),
+                ],
+                axis=1,
+            )
+    else:
+        raise NotImplementedError(type(recipe_tree))
---
+++ tests/renderer/test_recipe_to_table.py
@@ -0,0 +1,112 @@
+from typing import Mapping, Tuple, List, cast
+
+from recipe_grid.renderer.table import (
+    Cell,
+    Table,
+    BorderType,
+    combine_tables,
+    set_border_around_table,
+)
+
+from recipe_grid.recipe import RecipeTreeNode, Ingredient, Reference, Step, SubRecipe
+
+from recipe_grid.scaled_value_string import ScaledValueString as SVS
+
+from recipe_grid.renderer.recipe_to_table import recipe_tree_to_table
+
+
+def test_ingredient() -> None:
+    ingredient = Ingredient(SVS("spam"))
+    assert recipe_tree_to_table(ingredient) == Table.from_dict(
+        {(0, 0): Cell(ingredient)}
+    )
+
+
+def test_reference() -> None:
+    sub_recipe = SubRecipe(Ingredient(SVS("spam")), (SVS("out"),))
+    reference = Reference(sub_recipe, 0)
+    assert recipe_tree_to_table(reference) == Table.from_dict({(0, 0): Cell(reference)})
+
+
+def test_step() -> None:
+    input_0 = Ingredient(SVS("input 0"))
+    input_1 = Ingredient(SVS("input 1"))
+
+    input_2_ingredient = Ingredient(SVS("input 2"))
+    input_2 = Step(SVS("chopped"), (input_2_ingredient,))
+    step = Step(SVS("combine"), (input_0, input_1, input_2))
+
+    assert recipe_tree_to_table(step) == Table.from_dict(
+        cast(
+            Mapping[Tuple[int, int], Cell[RecipeTreeNode]],
+            {
+                (0, 0): Cell(input_0, columns=2),
+                (1, 0): Cell(input_1, columns=2),
+                (2, 0): Cell(input_2_ingredient),
+                (2, 1): Cell(input_2),
+                (0, 2): Cell(step, rows=3),
+            },
+        )
+    )
+
+
+def test_single_output_sub_recipe() -> None:
+    ingredient = Ingredient(SVS("spam"))
+    step = Step(SVS("fry"), (ingredient,))
+    sub_recipe = SubRecipe(step, (SVS("out"),))
+
+    assert recipe_tree_to_table(sub_recipe) == set_border_around_table(
+        Table.from_dict(
+            cast(
+                Mapping[Tuple[int, int], Cell[RecipeTreeNode]],
+                {
+                    (0, 0): Cell(sub_recipe, columns=2),
+                    (1, 0): Cell(ingredient),
+                    (1, 1): Cell(step),
+                },
+            )
+        ),
+        BorderType.sub_recipe,
+    )
+
+
+def test_multiple_output_sub_recipe() -> None:
+    ingredient_0 = Ingredient(SVS("spam"))
+    ingredient_1 = Ingredient(SVS("eggs"))
+    ingredient_2 = Ingredient(SVS("more spam"))
+    step = Step(SVS("fry"), (ingredient_0, ingredient_1, ingredient_2))
+    sub_recipe = SubRecipe(step, (SVS("output 0"), SVS("output 1")))
+
+    assert recipe_tree_to_table(sub_recipe) == combine_tables(
+        cast(
+            List[Table[RecipeTreeNode]],
+            [
+                set_border_around_table(
+                    Table.from_dict(
+                        cast(
+                            Mapping[Tuple[int, int], Cell[RecipeTreeNode]],
+                            {
+                                (0, 0): Cell(ingredient_0),
+                                (1, 0): Cell(ingredient_1),
+                                (2, 0): Cell(ingredient_2),
+                                (0, 1): Cell(step, rows=3),
+                            },
+                        )
+                    ),
+                    BorderType.sub_recipe,
+                ),
+                Table.from_dict(
+                    {
+                        (0, 0): Cell(
+                            sub_recipe,
+                            rows=3,
+                            border_top=BorderType.none,
+                            border_right=BorderType.none,
+                            border_bottom=BorderType.none,
+                        )
+                    }
+                ),
+            ],
+        ),
+        axis=1,
+    )
