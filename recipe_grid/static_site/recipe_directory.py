47c3bd614160a11053607c9f8dfb0f8c9fe1d502
Jonathan Heathcote <mail@jhnet.co.uk>
2020-09-29 10:17:37 +0100

    Clean up recipe directory listing submodule.

---
+++ recipe_grid/static_site/recipe_directory.py
@@ -0,0 +1,201 @@
+"""
+Utilities for enumerating categorised recipes within a directory hierarchy.
+
+Recipes may be arranged in a hierarchy of directories (for example) according
+to categories and subcategories. All files with a ``.md`` extension are assumed
+to be a recipe grid markdown file describing a single recipe.
+
+Directories may optionally contain an ``index.md`` or ``README.md`` file which
+better describes the directory contents. This markdown document must contain a
+H1 title at the start which will be used as the user-facing category name. When
+no such file is present, the category name will be a prettified version of the
+directory name (see :py:func:`dirname_to_title`).
+"""
+
+from typing import Tuple, NamedTuple, Optional, List
+
+from pathlib import Path
+
+from functools import lru_cache
+
+import re
+
+import marko  # type: ignore
+
+from html import unescape
+
+from peggie import ParseError
+from recipe_grid.compiler import RecipeCompileError
+
+from recipe_grid.markdown import compile_markdown, MarkdownRecipe
+
+from recipe_grid.static_site.exceptions import (
+    ReadmeMissingTitleError,
+    ReadmeMalformedTitleError,
+    RecipeInDirectoryCompileError,
+    RecipeMissingTitleError,
+    RecipeMissingServingsError,
+    MultipleReadmeError,
+)
+
+
+def dirname_to_title(filename: str) -> str:
+    """
+    Given a filename in snake case, camel case or containing Real Spaces(TM),
+    returns a normalised rendering. For example "myDirName", "MY_DIR_NAME" and
+    "My dir name" would all become "My dir name".
+    """
+    # Surround numbers in whitespace
+    filename = re.sub(r"[0-9]+", lambda match: f" {match.group(0)} ", filename,)
+
+    # Add spaces at camel-case word boundaries
+    filename = re.sub(
+        r"([^A-Z])([A-Z])",
+        lambda match: " ".join(g for g in match.groups() if g is not None),
+        filename,
+    )
+
+    # Replace all runs of punctuation/spaces into single spaces
+    filename = re.sub(r"[^a-zA-Z0-9]+", " ", filename)
+
+    # Strip leading/trailing spaces
+    filename = filename.strip()
+
+    # Normalise case
+    return " ".join(
+        word.title() if i == 0 else word.lower()
+        for i, word in enumerate(filename.split())
+    )
+
+
+def compile_readme_markdown(path: Path) -> Tuple[str, str]:
+    """
+    Read and compile a markdown 'README' document, stripping the <h1> heading
+    and returning the title and remainder of the document separately.
+
+    Returns
+    =======
+    title: str
+        The title (free from HTML escape sequences)
+    description: str
+        The remainder of the compiled markdown HTML source.
+    """
+
+    with path.open() as f:
+        html = marko.Markdown()(f.read())
+
+    lines = html.splitlines(keepends=True)
+
+    first_line = (lines[0] if lines else "").strip()
+    if not (first_line.startswith("<h1>") and first_line.endswith("</h1>")):
+        raise ReadmeMissingTitleError(f"{path} must start with a h1-level title.")
+
+    title = first_line[len("<h1>") : -len("</h1>")]
+    if "<" in title:
+        raise ReadmeMalformedTitleError(
+            f"{path} must have only simple text in its  h1 title"
+        )
+
+    title = unescape(title)
+    description = "".join(lines[1:])
+
+    return title, description
+
+
+_cached_compile_markdown = lru_cache(compile_markdown)
+"""Used in :py:func:`compile_recipe`."""
+
+
+def compile_recipe_markdown(
+    recipe_source: Path, require_title: bool = True, require_servings: bool = True
+) -> MarkdownRecipe:
+    """
+    Compile a recipe from source, converting compilation errors into
+    :py:exc:`~RecipeInDirectoryCompileError` and optionally throwing
+    :py:exc:`~RecipeMissingTitleError` and
+    :py:exc:`~RecipeMissingServingsError` if the title or serving count are not
+    given for the recipe.
+    """
+    with recipe_source.open() as f:
+        try:
+            # NB: Because we cache based on the markdown contents we will never
+            # produce a stale result.
+            recipe = _cached_compile_markdown(f.read())
+        except (RecipeCompileError, ParseError) as e:
+            raise RecipeInDirectoryCompileError(
+                f"Error while compiling {recipe_source}: {e}"
+            )
+    if require_title and recipe.title is None:
+        raise RecipeMissingTitleError(f"Recipe {recipe_source} is missing a title")
+    if require_servings and recipe.servings is None:
+        raise RecipeMissingServingsError(
+            f"Recipe {recipe_source} is missing a number of "
+            f"servings (e.g. '... for 3' in the title)"
+        )
+
+    return recipe
+
+
+class RecipeDirectoryListing(NamedTuple):
+    title: str
+    """
+    The user-facing title for this directory. Taken from the README if present,
+    otherwise generated from the directory name.
+    """
+
+    description_html: Optional[str]
+    """
+    If a README is present, the rendered markdown content, excluding the <h1>
+    block.
+    """
+
+    description_source: Optional[Path]
+    """If a README is present, its filename."""
+
+    subdirectories: List[Path]
+    """The subdirectories in this listing, in no particular order."""
+
+    recipes: List[Path]
+    """The recipes in this directory in no particular order."""
+
+
+def enumerate_recipe_directory(directory: Path) -> RecipeDirectoryListing:
+    """
+    Enumerate the contents of a recipe website directory.
+    """
+    if not directory.is_dir():
+        raise NotADirectoryError(f"{directory} is not a directory")
+
+    description_source: Optional[Path] = None
+    subdirectories: List[Path] = []
+    recipes: List[Path] = []
+
+    for path in directory.iterdir():
+        if path.is_dir():
+            subdirectories.append(path)
+        elif path.name.lower() in ("readme.md", "index.md"):
+            if description_source is None:
+                description_source = path
+            else:
+                raise MultipleReadmeError(
+                    f"{directory} contains multiple readme files: "
+                    f"{description_source.name} and {path.name}."
+                )
+        elif path.suffix.lower() == ".md":
+            recipes.append(path)
+
+    title: str
+    description_html: Optional[str]
+    if description_source is None:
+        title = dirname_to_title(directory.resolve().name)
+        description_html = None
+    else:
+        title, description_html = compile_readme_markdown(description_source)
+
+    return RecipeDirectoryListing(
+        title=title,
+        description_html=description_html,
+        description_source=description_source,
+        subdirectories=subdirectories,
+        recipes=recipes,
+    )
---
+++ tests/static_site/test_recipe_directory.py
@@ -0,0 +1,239 @@
+import pytest
+
+from pathlib import Path
+
+from textwrap import dedent
+
+from recipe_grid.static_site.exceptions import (
+    ReadmeMissingTitleError,
+    ReadmeMalformedTitleError,
+    RecipeMissingTitleError,
+    RecipeMissingServingsError,
+    RecipeInDirectoryCompileError,
+    MultipleReadmeError,
+)
+
+from recipe_grid.static_site.recipe_directory import (
+    dirname_to_title,
+    compile_readme_markdown,
+    compile_recipe_markdown,
+    RecipeDirectoryListing,
+    enumerate_recipe_directory,
+)
+
+
+@pytest.mark.parametrize(
+    "dirname, exp",
+    [
+        # Empty
+        ("", ""),
+        # Whitespace only
+        (" ", ""),
+        # Single word
+        ("foo", "Foo"),
+        ("FOO", "Foo"),
+        ("123", "123"),
+        # Multiple words
+        ("fooBar", "Foo bar"),
+        ("foo_bar", "Foo bar"),
+        ("FOO_BAR", "Foo bar"),
+        # With numbers
+        ("foo123", "Foo 123"),
+        ("FOO_123", "Foo 123"),
+        ("123foo", "123 foo"),
+        ("123_foo", "123 foo"),
+        ("foo123bar", "Foo 123 bar"),
+        ("foo2bar", "Foo 2 bar"),
+        # With real spaces
+        ("foo bar baz", "Foo bar baz"),
+        # With excess spaces
+        ("  foo    bar  ", "Foo bar"),
+        # With other misc punctuation
+        ("foo@bar", "Foo bar"),
+    ],
+)
+def test_filename_to_title(dirname: str, exp: str) -> None:
+    assert dirname_to_title(dirname) == exp
+
+
+class TestCompileReadmeMarkdown:
+    @pytest.mark.parametrize(
+        "markdown",
+        [
+            # Empty
+            "",
+            # No title
+            "Hello",
+            # Title is not h1
+            "## Hello",
+            # H1 title is not first
+            "## Hello\n# World",
+            "Hello\n# World",
+        ],
+    )
+    def test_no_h1_title(self, tmp_path: Path, markdown: str) -> None:
+        md = tmp_path / "test.md"
+        md.open("w").write(markdown)
+        with pytest.raises(ReadmeMissingTitleError):
+            compile_readme_markdown(md)
+
+    def test_title_contains_html(self, tmp_path: Path) -> None:
+        md = tmp_path / "test.md"
+        md.open("w").write("# Hello *world*")
+        with pytest.raises(ReadmeMalformedTitleError):
+            compile_readme_markdown(md)
+
+    @pytest.mark.parametrize(
+        "markdown, exp_title, exp_description",
+        [
+            # Just title
+            ("# Hello", "Hello", ""),
+            # Contains HTML escapes
+            ("# Foo & Bar", "Foo & Bar", ""),
+            # Title and description
+            ("# Hello\nWorld\n\nHooray!", "Hello", "<p>World</p>\n<p>Hooray!</p>\n"),
+        ],
+    )
+    def test_valid(
+        self, tmp_path: Path, markdown: str, exp_title: str, exp_description: str
+    ) -> None:
+        md = tmp_path / "test.md"
+        md.open("w").write(markdown)
+        title, description = compile_readme_markdown(md)
+        assert title == exp_title
+        assert description == description
+
+
+class TestCompileRecipeMarkdown:
+    def test_recipe_missing_title(self, tmp_path: Path) -> None:
+        f = tmp_path / "recipe.md"
+        f.open("w").write("A title-less recipe")
+        with pytest.raises(RecipeMissingTitleError):
+            compile_recipe_markdown(f)
+
+        # Shouldn't crash
+        compile_recipe_markdown(f, require_title=False, require_servings=False)
+
+    def test_recipe_missing_servings(self, tmp_path: Path) -> None:
+        f = tmp_path / "recipe.md"
+        f.open("w").write("# A serving-less recipe")
+        with pytest.raises(RecipeMissingServingsError):
+            compile_recipe_markdown(f)
+
+        # Shouldn't crash
+        compile_recipe_markdown(f, require_servings=False)
+
+    def test_recipe_syntax_error(self, tmp_path: Path) -> None:
+        f = tmp_path / "recipe.md"
+        f.open("w").write(
+            dedent(
+                """
+                    # Fail for 1
+                    ```recipe
+                    Missing a ( closing bracket...
+                    ```
+                """
+            )
+        )
+        with pytest.raises(RecipeInDirectoryCompileError):
+            compile_recipe_markdown(f)
+
+    def test_recipe_compile_error(self, tmp_path: Path) -> None:
+        f = tmp_path / "recipe.md"
+        f.open("w").write(
+            dedent(
+                """
+                    # Fail for 1
+                    ```recipe
+                    1/2 of undefined sub recipe
+                    ```
+                """
+            )
+        )
+        with pytest.raises(RecipeInDirectoryCompileError):
+            compile_recipe_markdown(f)
+
+    def test_valid(self, tmp_path: Path) -> None:
+        f = tmp_path / "recipe.md"
+        f.open("w").write("# A recipe for 2")
+        r = compile_recipe_markdown(f)
+
+        assert r.title == "A recipe"
+        assert r.servings == 2
+
+    def test_cache_transparency(self, tmp_path: Path) -> None:
+        f = tmp_path / "recipe.md"
+        f.open("w").write("# A recipe for 2")
+
+        r1 = compile_recipe_markdown(f)
+        r2 = compile_recipe_markdown(f)
+
+        # Result from cache
+        assert r2 is r1
+
+        # Changing the file on disk should return newly compiled output
+        f.open("w").write("# A different recipe for 3")
+        r3 = compile_recipe_markdown(f)
+        assert r3.title == "A different recipe"
+        assert r3.servings == 3
+
+
+class TestEnumerateRecipeDirectory:
+    def test_not_exists(self, tmp_path: Path) -> None:
+        path = tmp_path / "not_exists"
+        with pytest.raises(NotADirectoryError):
+            enumerate_recipe_directory(path)
+
+    def test_not_a_directory(self, tmp_path: Path) -> None:
+        path = tmp_path / "not_exists"
+        path.open("w").write("")
+        with pytest.raises(NotADirectoryError):
+            enumerate_recipe_directory(path)
+
+    def test_empty(self, tmp_path: Path) -> None:
+        empty_dir = tmp_path / "empty_dir"
+        empty_dir.mkdir()
+        assert enumerate_recipe_directory(empty_dir) == RecipeDirectoryListing(
+            title="Empty dir",
+            description_html=None,
+            description_source=None,
+            subdirectories=[],
+            recipes=[],
+        )
+
+    @pytest.mark.parametrize(
+        "readme_filename", ["index.md", "INDEX.md", "readme.md", "README.md"],
+    )
+    def test_readme(self, tmp_path: Path, readme_filename: str) -> None:
+        path = tmp_path / "test_dir"
+        path.mkdir()
+
+        readme = path / readme_filename
+        readme.open("w").write("# A Directory\nTa-da!")
+
+        assert enumerate_recipe_directory(path) == RecipeDirectoryListing(
+            title="A Directory",
+            description_html="<p>Ta-da!</p>\n",
+            description_source=readme,
+            subdirectories=[],
+            recipes=[],
+        )
+
+    def test_multiple_index_files(self, tmp_path: Path) -> None:
+        (tmp_path / "index.md").open("w").write("# Hello")
+        (tmp_path / "readme.md").open("w").write("# Hello")
+        with pytest.raises(MultipleReadmeError):
+            enumerate_recipe_directory(tmp_path)
+
+    def test_recipes(self, tmp_path: Path) -> None:
+        path = tmp_path / "test_dir"
+        path.mkdir()
+
+        (path / "foo.md").open("w").write("# Foo for 2")
+        (path / "bar.md").open("w").write("# Bar for 3")
+
+        d = enumerate_recipe_directory(path)
+
+        assert len(d.recipes) == 2
+
+        assert set(d.recipes) == {path / "foo.md", path / "bar.md"}
