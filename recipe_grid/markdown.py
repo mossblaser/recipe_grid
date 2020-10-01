b84a7ccfd5e0dc08357a3a278d5a71f146e92ec4
Jonathan Heathcote <mail@jhnet.co.uk>
2020-10-01 10:48:25 +0100

    Remove never-matching 'to serve' preposition.

--- recipe_grid/markdown.py
+++ recipe_grid/markdown.py
@@ -459,7 +459,7 @@ class RecipeGridRendererMixin:
     title_serving_count_pattern = re.compile(
         (
             r"(?P<title>.*)"
-            r"(?P<preposition>((to\s+)?serves?|for|makes|serving)\s+)"
+            r"(?P<preposition>(serves?|for|makes|serving)\s+)"
             r"(?P<servings>[0-9]+)\s*"
         ),
         re.IGNORECASE,
